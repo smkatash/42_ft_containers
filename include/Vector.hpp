1f43946448b9cbef76fcc61414e25160b1ca534a
Kanykei Tashbaeva <tashbaevakanykei@gmail.com>
2022-11-24 19:08:51 +0100

    update

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -1,6 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktashbae <ktashbae@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 18:18:23 by ktashbae          #+#    #+#             */
+/*   Updated: 2022/11/24 18:28:58 by ktashbae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
+#include <memory>
 
+namespace ft
+{
+	template <class T, class Allocator = allocator<T> >
+	class vector
+	{
+		public:
+		typedef T											value_type;
+		typedef Allocator									allocator_type;
+		typedef typename allocator_type::reference			reference;
+		typedef typename allocator_type::const_reference	const_reference;
+		typedef typename allocator_type::pointer			pointer;
+		typedef typename allocator_type::const_pointer		const_pointer;
+		typedef typename allocator_type::size_type			size_type;
 
+		typedef ft::iterator<T>											iterator;
+		typedef ft::iterator<const T>									const_iterator;
+		typedef ft::reverse_iterator<iterator>							reverse_iterator;
+		typedef ft::reverse_iterator<const_iterator>					const_reverse_iterator;
+		typedef typename iterator_traits<iterator>::difference_type		difference_type;
+
+		// Constructors ======================================================================//
+
+		//(1) empty container constructor (default constructor)
+		
+
+	};
+};
 #endif
--- test.cpp
+++ test.cpp
@@ -1,6 +1,15 @@
 #include <vector>
+#include <iostream>
+// #include "vector.hpp"
 
 int main()
 {
-    
+	// ft::vector<int> vector_ft;
+	std::vector<int> vec{2, 5, 20, 55, 5};
+	std::vector<int>::iterator ptr;
+
+	for (ptr = vec.begin(); ptr < vec.end(); ptr++)
+	{
+		std::cout << *ptr << " ";
+	}
 }
