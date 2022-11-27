bb759af69b04cdbf4067ce476dd968f411459dde
smkatash <tashbaevakanykei@gmail.com>
2022-11-27 20:31:51 +0100

    iterator wip

--- include/iterator.hpp
+++ include/iterator.hpp
@@ -5,16 +5,23 @@
 
 namespace ft
 {
-	// Iterator_trait ===================================================//
+	// Iterator_traits ===================================================//
 	// src: https://www.codeproject.com/Articles/36530/An-Introduction-to-Iterator-Traits
-	template <class T>
+/**
+ * Returns basic information about an iterator.
+ * The iterator_traits template and specialization 
+ * allows algorithms to access information about a particular iterator 
+ * in a uniform way. 
+ */
+	template <class Iterator>
 	struct iterator_traits {
-		typedef typename T::value_type				value_type;
-		typedef typename T::difference_type			difference_type;
-		typedef typename T::iterator_category		iterator_category;
-		typedef typename T::pointer					pointer;
-		typedef typename T::reference				reference;
+		typedef typename Iterator::value_type				value_type;
+		typedef typename Iterator::difference_type			difference_type;
+		typedef typename Iterator::iterator_category		iterator_category;
+		typedef typename Iterator::pointer					pointer;
+		typedef typename Iterator::reference				reference;
 	};
+
 /**
  * ptrdiff_t : Like in size_t, ptrdiff_t can safely store a pointer except 
  * for a pointer to a class function. Also, ptrdiff_t is the type of the result 
@@ -31,7 +38,6 @@ namespace ft
  * Bidirectional_iterator_tag corresponds to BidirectionalIterator.
  * Random_access_iterator_tag corresponds to RandomAccessIterator.
  */
-
 	template <class T>
 	struct iterator_traits<T*> {
 		typedef T									value_type;
@@ -42,25 +48,144 @@ namespace ft
 	};
 
 	template <class T>
-	struct iterator_traits< const T*> {
+	struct iterator_traits<const T*> {
 		typedef T									value_type;
 		typedef ptrdiff_t							difference_type;
 		typedef std::random_access_iterator_tag		iterator_category;
 		typedef T*									pointer;
 		typedef T&									reference;
 	};
-
+/**
+ * distance() for input, forward, and bidirectional iterator
+ * Input iterator tag only is used, forward and bidirectional iterator 
+ * are also be in this bucket, because they inherit from InputIterator.
+ */
 	template<typename InputIterator>
 	typename iterator_traits<InputIterator>::difference_type distance(InputIterator first, InputIterator last, \
 	typename ft::enable_if<!ft::is_integral<InputIterator>::value>::type* = nullptr)
 	{
-		typename iterator_traits<InputIterator>::difference_type n = 0;
+		typename iterator_traits<InputIterator>::difference_type diff = 0;
 		while (first != last) {
-			++n;
 			++first;
+			++diff;
 		};
-		return n;
+		return diff;
 	}
+
+	// Iterators ===================================================//
+
+	template <class T>
+	class iterator
+	{
+		public:
+			typedef T									value_type;
+			typedef ptrdiff_t							difference_type;
+			typedef std::random_access_iterator_tag		iterator_category;
+			typedef T*									pointer;
+			typedef const T*							const_pointer;
+			typedef T&									reference;
+			typedef const T&							const_reference;
+			typedef iterator <const T>					const_iterator;
+
+			iterator(void) : 				_it()			{};
+			iterator(pointer it) : 			_it(it)			{};
+			iterator(const iterator& it) : _it(it._it)		{};
+			~iterator(void)									{};
+
+			pointer			base() const { 
+				return _it;
+			};
+
+			/** Assignment operators
+			 */
+			iterator		operator=(const iterator& rhs)			{
+				this->_it = rhs.base();
+				return *this;
+			};
+
+			iterator&		operator+=(difference_type diff)		{
+				this->_it += diff;
+				return *this;
+			};
+
+			iterator&		operator-=(difference_type diff)		{
+				this->_it -= diff;
+				return *this;
+			};
+
+			/** Arithmetic operators
+			 */
+			iterator		operator+(difference_type diff) const	{
+				return iterator(base() + diff);
+			};
+
+			iterator		operator-(difference_type diff) const	{
+				return iterator(base() - diff);
+			};
+
+			/** Increment and Decrement operators
+			 */
+			iterator&		operator++()							{
+				this->_it += 1;
+				return *this;
+			};
+
+			iterator&		operator--()							{
+				this->_it -= 1;
+				return *this;
+			};
+
+			iterator		operator++(int)							{
+				iterator	tmp(*this);
+				this->_it += 1;
+				return tmp;
+			};
+
+			iterator		operator--(int)							{
+				iterator	tmp(*this);
+				this->_it -= 1;
+				return tmp;
+			};
+
+			/** Dereference operators
+			 */
+			reference		operator*()								{
+				return *(this->_it);
+			};
+
+			const_reference	operator*() const 						{
+				return *(this->_it);
+			};
+
+			/** Pointer operators
+			 */
+			pointer			operator->()							{
+				return &(operator*());
+			}
+
+			const_pointer	operator->() const						{
+				return &(operator*());
+			}
+
+			/** Array Index Operator
+			 */
+			reference		operator[] (difference_type n)			{
+				return base()[n];
+			};
+
+			const_reference	operator[] (difference_type n) const	{
+				return base()[n];
+			};
+
+			/** Const Iterator
+			 */
+			operator		const_iterator () const					{
+				return const_iterator(this->_it);
+			};
+
+		private:
+			pointer	_it;
+	};
 }
 
 #endif
--- test_concepts/enable_if.cpp
+++ test_concepts/enable_if.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <type_traits>
+#include <iterator_traits>
+#include <iterator>
 
 /*
  * Enable-if
